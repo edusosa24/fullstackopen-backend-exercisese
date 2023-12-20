260f48cae2e7190b44442be31539d7f13a5dba2b
Eduardo Sosa <sm.edu33@gmail.com>
2023-12-20 16:39:10 -0300

    Part 3 Exercises 13 to 18

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-let persons = require('./data.json');
+require('dotenv').config();
+const mongoose = require('mongoose');
+const Person = require('./src/database/models/person');
+const { errorHandler } = require('./src/utils/middleware/errorHandler');
 
 const app = express();
-app.use(express.static('build'));
 const PORT = process.env.PORT || 3001;
 
+app.use(express.static('build'));
 app.use(express.json());
 app.use(cors());
 
@@ -32,34 +35,56 @@ app.use(
   })
 );
 
-app.get('/api/persons', (req, res) => {
-  return res.json({ persons });
+const dbConnection = () => {
+  const mongoURL = process.env.MONGO_URI;
+  mongoose.set('strictQuery', false);
+  console.log('connecting to', mongoURL);
+
+  mongoose
+    .connect(mongoURL)
+    .then((result) => {
+      console.log('connected to MongoDB');
+    })
+    .catch((error) => {
+      console.log('error connecting to MongoDB:', error.message);
+    });
+};
+
+dbConnection();
+
+app.get('/api/persons', (req, res, next) => {
+  Person.find({})
+    .then((persons) => {
+      return res.json(persons);
+    })
+    .catch((error) => next(error));
 });
 
-app.get('/api/persons/:id', (req, res) => {
-  const id = +req.params.id;
-  const personInfo = persons.find((person) => person.id === id);
-
-  if (!personInfo) {
-    return res.status(404).json({
-      error: `Person with id ${id} was not found`,
-    });
-  }
-
-  res.json(personInfo);
+app.get('/api/persons/:id', (req, res, next) => {
+  const id = req.params.id;
+  Person.findById(id)
+    .then((person) => {
+      if (!person) {
+        return res.status(404).json({
+          error: `Person with id ${id} was not found`,
+        });
+      }
+      return res.json(person);
+    })
+    .catch((error) => next(error));
 });
 
 app.get('/api/info', (req, res) => {
-  const peopleNumber = persons.length;
-  const timestamp = new Date().toUTCString();
-
-  return res.send(`
+  Person.find({}).then((persons) => {
+    const timestamp = new Date().toUTCString();
+    return res.send(`
     <div>
-      <p>This phonebook has currently the info of ${peopleNumber} people </p>
+      <p>This phonebook has currently the info of ${persons.length} people </p>
       <br />
       <p>${timestamp}</p>
     </div>
     `);
+  });
 });
 
 app.post('/api/persons', (req, res) => {
@@ -75,39 +100,50 @@ app.post('/api/persons', (req, res) => {
     });
   }
 
-  const personExists = persons.find(
-    (person) => String(person.name) === String(req.body.name)
-  );
+  Person.findOne({ name: req.body.name })
+    .then((person) => {
+      if (person !== null) {
+        return res.status(400).json({
+          error: 'The person name already exists',
+        });
+      }
 
-  if (personExists) {
-    return res.status(400).json({
-      error: 'The person name already exists',
-    });
-  }
-
-  const id = Math.floor(Math.random() * (100000 - 1) + 1);
-
-  const person = {
-    id: id,
-    name: req.body.name,
-    number: req.body.number,
-  };
-
-  persons = persons.concat(person);
-
-  return res.status(201).json({
-    message: 'Person created correctly',
-    person: person,
-  });
+      const newPerson = new Person({
+        name: req.body.name,
+        number: req.body.number,
+      });
+      newPerson.save().then((response) => {
+        return res.status(201).json(newPerson);
+      });
+    })
+    .catch((error) => console.log(error.message));
 });
 
 app.delete('/api/persons/:id', (req, res) => {
-  const id = +req.params.id;
+  const id = req.params.id;
 
-  persons = persons.filter((person) => person.id !== id);
-
-  return res.status(204).send();
+  Person.findByIdAndDelete(id)
+    .then(() => {
+      return res.status(204).send();
+    })
+    .catch((error) => console.log(error.message));
 });
+
+app.put('/api/persons/:id', (req, res) => {
+  const id = req.params.id;
+
+  if (!req.body.number) {
+    return res.status(400).json({ error: 'A number is required.' });
+  }
+
+  Person.findByIdAndUpdate(id, { number: req.body.number }, { new: true })
+    .then((response) => {
+      return res.status(200).json(response);
+    })
+    .catch((error) => console.log(error.message));
+});
+
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
